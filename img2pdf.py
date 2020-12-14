81fba750446fa4d8441b811b5c6cc622e9632c7f
Sam Escott <6706111+sme272@users.noreply.github.com>
2020-12-14 22:57:36 +0100

    Added recursive folder search option

--- img2pdf.py
+++ img2pdf.py
@@ -1,13 +1,58 @@
+#!/bin/python3.8
 import os
 import sys
 from PIL import Image
+import argparse
 
-filelist = [i for i in os.listdir('./') if (i.endswith(".jpg") or i.endswith(".png"))]
-imagelist = []
 
-for image in filelist:
-	 img = Image.open(image)
-	 img = img.convert('RGB')
-	 imagelist.append(img)
+def recursive_compile(path):
+    image_list = []
+    for file in os.listdir(path):
+        if os.path.isdir(file):
+            new_path = os.path.join(path, file)
+            image_list.extend(recursive_compile(new_path))
+        elif file.lower().endswith(".jpg") or file.lower().endswith(".png"):
+            file = os.path.join(path, file)
+            img = Image.open(file)
+            img = img.convert("RGB")
+            image_list.append(img)
 
-imagelist[0].save(f'./{sys.argv[1]}.pdf', save_all=True, append_images=imagelist[1:])
+    return image_list
+
+
+def compile(path):
+    image_list = []
+    for file in os.listdir(path):
+        if file.lower().endswith(".jpg") or file.lower().endswith(".png"):
+            img = Image.open(file)
+            img = img.convert("RGB")
+            image_list.append(img)
+
+    return image_list
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Compiles all pdf's in a folder to a pdf"
+    )
+    parser.add_argument(
+        "-r",
+        "--recursive",
+        action="store_true",
+        help="Recursively compiles all images in all subfolders into a pdf",
+    )
+    parser.add_argument("pdf_name")
+    args = parser.parse_args()
+
+    if args.recursive:
+        image_list = recursive_compile("./")
+    else:
+        image_list = compile("./")
+
+    if not args.pdf_name.endswith(".pdf"):
+        args.pdf_name += ".pdf"
+
+    if len(image_list) > 1:
+        image_list[0].save(args.pdf_name, save_all=True, append_images=image_list[1:])
+    elif len(image_list) == 1:
+        image_list[0].save(args.pdf_name, save_all=True)
